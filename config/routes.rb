f70c8b61b452587025318c423be9e88bfac5030f
Nga Nguyen <nganguyen.ny@gmail.com>
2019-07-30 15:00:45 +0800

    Implement mini task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # CREATE
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  # READ
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # resources :tasks
 end
